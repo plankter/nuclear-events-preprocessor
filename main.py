af2fbce2cd39055e232c16158c89c0cda90dec59
Anton Rau <anton.rau@gmail.com>
2018-09-17 16:58:50 +0200

    Preprocessed data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from geopy.extra.rate_limiter import RateLimiter
 
 geolocator = Nominatim(user_agent="Nucleus")
 
+
 def preprocess(df):
     df.dropna(axis=0, subset=['EventNumber'], inplace=True)
     df['EventNumber'] = df.EventNumber.astype(int)
@@ -15,15 +16,38 @@ def preprocess(df):
 
     dates = pd.to_datetime(dict(year=df.Year, month=df.Month, day=df.Day))
     df.insert(1, "Date", dates)
-    df.drop(columns=['Year', 'Month', 'Day'], inplace=True)
+
+    df['Year'] = df.EventNumber.astype(int)
+    df['Month'] = df.EventNumber.astype(int)
+    df['Day'] = df.EventNumber.astype(int)
+
+    df.rename(columns={"EventNumber": "event_number", "Geographical Location": "location",
+                       "INES (guess)": "ines_guess", "Gross Electrical Capacity [MW]": "capacity",
+                       "Grid Connection Year": "connection_year", "Comments (DB V3)": "comments",
+                       "Core relevant": "core_relevant", "Origin_description": "origin_description",
+                       "Origin (incl. potential origin in the case of a degraded system)": "origin",
+                       "Cause_description": "cause_description",
+                       "Cause (incl. potential cause)": "cause", "Lower Limit": "lower_limit",
+                       "Upper Limit": "upper_limit"}, inplace=True)
+    df.rename(str.lower, axis='columns')
     return df
 
+
 def get_location(df):
-    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-    locations = df.head()['Geographical Location'].apply(geocode)
-    print(locations)
-    #df['Latitude'] = locations.Latitude
-    #df['Longitude'] = locations.Longitude
+    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=0.1)
+    latitude = []
+    longitude = []
+    for row in df.location:
+        location = geocode(row)
+        if location is not None:
+            latitude.append(location.latitude)
+            longitude.append(location.longitude)
+        else:
+            latitude.append(None)
+            longitude.append(None)
+
+    df.insert(6, "latitude", latitude)
+    df.insert(7, "longitude", longitude)
     return df
 
 
@@ -32,4 +56,6 @@ df = pd.read_excel('data/data.xlsx')  # sheetname is optional
 df = preprocess(df)
 df = get_location(df)
 
-df.to_csv('data/data.csv', index=False)  # index=False prevents pandas to write row index
+df.to_pickle('data/output.pkl')
+df.to_csv('data/output.csv', index=False, sep='\t', escapechar='\\')  # index=False prevents pandas to write row index
+df.to_json('data/output.json', date_format='iso')
